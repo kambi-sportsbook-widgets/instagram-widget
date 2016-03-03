59e4df61df24e76afefb7a03ef0e55100bd42203
Rodrigo Carrasco <rodrigo@globalmouth.com>
2016-03-03 13:00:56 +0100

    KSBWI-191: Getting the aspect ratio of the image and resizing the widget accordingly

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -2,10 +2,10 @@
 
    'use strict';
 
-   (function ( $app ) {
+   (function ($app) {
       return $app.controller('appController',
          ['$scope', '$controller', '$http', '$sce', 'kambiAPIService', 'kambiWidgetService',
-            function ( $scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService ) {
+            function ($scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService) {
 
                angular.extend(this, $controller('widgetCoreController', {
                   '$scope': $scope
@@ -14,16 +14,24 @@
                // Default arguments, these will be overridden by the arguments from the widget api
                $scope.defaultArgs = {
                   'instagram': {
-                     'href': 'https://www.instagram.com/p/BCGuqy1CogW'
+                     'href': 'https://www.instagram.com/p/BARTu05AvwB'
                   }
                };
 
-               var kwcard = $('.kw-card');
-               console.log(kwcard);
+               // Getting the image's width and height from json
+               var instagramApiUrl = 'http://api.instagram.com/publicapi/oembed/?url=';
+               $http.jsonp(instagramApiUrl + $scope.defaultArgs.instagram.href + '/&callback=JSON_CALLBACK').success(function (data) {
+                  $scope.pictureWidth = data.thumbnail_width;
+                  $scope.pictureHeight = data.thumbnail_height;
+               });
 
-               $(window).bind('resize', function() {
+               // Variables for resizing according to the aspect ratio of the image
+               var kwcard = $('.kw-card');
+               var instagramHeaderFooter = 88;
+
+               $(window).bind('resize', function () {
                   $scope.width = kwcard.width();
-                  $scope.setWidgetHeight($scope.width + 80);
+                  $scope.setWidgetHeight(($scope.pictureHeight / $scope.pictureWidth) * $scope.width + instagramHeaderFooter);
                });
 
                // Call the init method in the coreWidgetController so that we setup everything using our overridden values
